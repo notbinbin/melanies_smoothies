3488efb131115267e39dbae7070bedb258fb4645
notbinbin <117302506+notbinbin@users.noreply.github.com>
2025-02-04 17:06:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 
 
-session = get_active_session()
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
