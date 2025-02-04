8c41899802e8ff0232d49c1a70c00e8caec6c773
notbinbin <117302506+notbinbin@users.noreply.github.com>
2025-02-05 07:40:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 # Import python packages
 import streamlit as st
-import request
-import panda as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -56,11 +54,6 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
 
+import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-
-if smoothiefroot_response.status_code == 200:
-    data = smoothiefroot_response.json()  # Convert response to JSON
-    sf_df = pd.DataFrame([data])  # Convert JSON to DataFrame
-    st.dataframe(sf_df)  # Display in Streamlit
-else:
-    st.error(f"API request failed with status code {smoothiefroot_response.status_code}")
+st.text(smoothiefroot_response)
