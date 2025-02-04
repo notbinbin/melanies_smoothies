5aae5295d3e7faafac2b11f2e4de5aa6d9d35010
notbinbin <117302506+notbinbin@users.noreply.github.com>
2025-02-04 16:58:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw:")
@@ -10,6 +11,9 @@ st.write(
     """
 )
 
+name_on_order = st.text_input("Name on Smoothie")
+st.write("The name on your Smoothie will be:", name_on_order)
+
 # option = st.selectbox(
     # "What is your Favorite Fruit?",
     # ("Banana", "Strawberries", "Peaches"),
@@ -17,23 +21,16 @@ st.write(
 
 # st.write("Your favorite fruit is:", option)
 
-from snowflake.snowpark.functions import col
 
-cnx = st.connection("snowflake",
-                    account = "BMQLMVK-YI48100",
-                    user = "gabalesvin",
-                    password = "Gabales091698!",
-                    role = "SYSADMIN",
-                    warehouse = "COMPUTE_WH",
-                    database = "SMOOTHIES",
-                    schema = "PUBLIC")
-session = cnx.session()
+
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
+    , max_selections=5
 )
 if ingredients_list:
     # st.write(ingredients_list)
@@ -42,14 +39,16 @@ if ingredients_list:
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
+        ingredients_string += fruit_chosen + ''
 
     # st.write(ingredients_string)
 
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
-# st.write(my_insert_stmt)
+    # st.write(my_insert_stmt)
+    # st.stop()
+
 time_to_insert = st.button('Submit Order')
 
 if time_to_insert:
