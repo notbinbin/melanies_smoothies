e81a460e29fbf058e5bf3949bbd6036205c02215
notbinbin <117302506+notbinbin@users.noreply.github.com>
2025-02-05 08:00:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -56,7 +57,7 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
+
 
 # st.text(smoothiefroot_response.json())
 
